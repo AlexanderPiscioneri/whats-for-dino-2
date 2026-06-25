57692ae283222fd3dc01bc0389f8b2006f438a3d
AlexanderPiscioneri <AlexanderPiscioneri@users.noreply.github.com>
2026-06-25 23:29:03 +0000

    deploy: ec06c88f36cc733c84713197f6663c9b6bd17f3f

--- main.dart.js
+++ main.dart.js
@@ -32804,7 +32804,7 @@ $S:2}
 A.aHJ.prototype={
 $1(a){var s=A.dG().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
 $S:36}
 A.T6.prototype={
 gq(a){var s=this.a
